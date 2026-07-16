535e0ef0f539e7de79ac16cc6b48a5d121efbc17
Kenji Ishikawa <ishikawa.kenji@ditgroup.jp>
2026-07-16 11:26:33 +0900

    Fix GitHub Pages redirect URI

--- src/auth/msalConfig.ts
+++ src/auth/msalConfig.ts
@@ -3,7 +3,12 @@ export const msalConfig = {
     clientId: "51913406-3d5a-44bc-847c-48f21f21c623",
     authority:
       "https://login.microsoftonline.com/e9b67710-82df-42cc-9329-96d07c440be2",
-    redirectUri: window.location.origin,
+
+    redirectUri:
+  import.meta.env.DEV
+    ? "http://localhost:5173"
+    : "https://ditishikawakenji.github.io/TeamScheduleBoard/",
+
     navigateToLoginRequestUrl: false,
   },
   cache: {
@@ -17,6 +22,6 @@ export const loginRequest = {
     "User.Read",
     "Calendars.Read",
     "Calendars.Read.Shared",
-    "User.ReadBasic.All"
-  ]
+    "User.ReadBasic.All",
+  ],
 };
